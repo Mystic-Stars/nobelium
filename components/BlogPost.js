21667ebe481bd27261154b157ad0c5309c29ef43
Mystic Stars <97295993+Mystic-Stars@users.noreply.github.com>
2024-08-29 13:56:28 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -3,26 +3,26 @@ import { useConfig } from "@/lib/config";
 import Link from "next/link";
 
 const BlogPost = ({ post }) => {
-  const BLOG = useConfig();
-
-  return (
-    <Link href={`${BLOG.path}/${post.slug}`}>
-      <article key={post.id} className="mb-6 md:mb-8">
-        <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-          <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
-            {post.title}
-          </h2>
-          <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
-            <FormattedDate date={post.date} />
-          </time>
-        </header>
-        <main>
-          <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
-            {post.summary}
-          </p>
-        </main>
-      </article>
-    </Link>
+   return (
+     <Link href={`${BLOG.path}/${post.slug}`}>
+       <a>
+         <article key={post.id} className="mb-6 md:mb-8">
+           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
+             <h2 className="text-lg md:text-xl font-bold mb-2 cursor-pointer text-black dark:text-gray-200">
+               {post.title}
+             </h2>
+             <time className="flex-shrink-0 text-gray-600 dark:text-gray-500">
+               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
+             </time>
+           </header>
+           <main>
+             <p className="hidden md:block leading-7 text-gray-700 dark:text-gray-400">
+               {post.summary}
+             </p>
+           </main>
+         </article>
+       </a>
+     </Link>
   );
 };
 
